344a1c42a96e5ebed70cc141bb5bc58022fa7bb4
Elena <shipkovalena@gmail.com>
2021-03-21 12:15:15 +0300

    Cash and calorie Calc edited after the review1

--- homework.py
+++ homework.py
@@ -1,106 +1,123 @@
-import datetime as dt
-from typing import List, Union
+"""Money and Calorie Calculator."""
 
+import datetime as dt
+from typing import List
+
+date_format = '%d.%m.%Y'
 
 class Record:
-    def __init__(self, amount, comment, date=None) -> None:
+    """Class Record to define the format of user spending records."""
+
+    def __init__(self, amount: int, comment: str, date=None) -> None:
+        """Set the required attributes for the record object.
+
+        Attributes:
+        amount (int): amount of spend money or calories consumed
+        comment (str): user's text about the amounts
+        date (None): if date is None, assign default value - current date
+        """
+
         self.amount = amount
         self.comment = comment
         if date is not None:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+            self.date = dt.datetime.strptime(date, date_format).date()
         else:
             self.date = dt.date.today()
 
-    def show_record(self):
-        return f'{self.amount}, {self.date}, {self.comment}'
-
 
 class Calculator:
-    def __init__(self, limit: float) -> None:
+    """Class Calculator - application for calculating expences and calories."""
+
+    def __init__(self, limit: int) -> None:
+        """Set the required attributes for the calculator object.
+        
+        Attributes:
+        limit (int): the amount of daily spending/calorie limit set by the user
+        records (List[Record]): list for storing records of expenses amounts or calories
+        today (dt.date.today()): current date
+        """
+
         self.limit = limit
-        self.records: List[Union[str, float, dt.date]] = []
+        self.records: List[Record] = []
+        self.today = dt.date.today()
 
     def add_record(self, record):
+        """Method add_record() to add record to the list records."""
+
         self.records.append(record)
 
-    def get_today_stats(self):
-        current_mom = dt.date.today()
-        current_mom_amount = 0
-        for record in self.records:
-            if record.date == current_mom:
-                current_mom_amount += record.amount
-        return current_mom_amount
+    def get_today_stats(self) -> int:
+        """Method get_today_stats() to calculate expences/calories for today."""
 
-    def get_today_remained(self):
-        remained = round((self.limit - self.get_today_stats()), 2)
+        today_stats = []
+        for record in self.records:
+            if record.date == self.today:
+                today_stats.append(record.amount)
+        return sum(today_stats)
+
+    def get_today_remained(self) -> float:
+        """Method get_today_remained() to calculate the available cash/calorie limit."""
+
+        remained = self.limit - self.get_today_stats()
         return remained
 
-    def get_week_stats(self):
-        current_mom = dt.date.today()
-        last_week = current_mom - dt.timedelta(days=6)
-        last_week_amount = 0
+    def get_week_stats(self) -> int:
+        """Method get_week_stats() to calculate the amount of money spent/calories for the week."""
+
+        week_ago = self.today - dt.timedelta(days=7)
+        week_stats = []
         for record in self.records:
-            if last_week <= record.date <= current_mom:
-                last_week_amount += record.amount
-        return last_week_amount
+            if week_ago < record.date <= self.today:
+                week_stats.append(record.amount)
+        return sum(week_stats)
 
 
 class CaloriesCalculator(Calculator):
-    def __init__(self, limit: float) -> None:
-        super().__init__(limit)
+    """Class CaloriesCalculator extended class Calculator."""
 
-    def get_calories_remained(self):
+    def get_calories_remained(self) -> str:
+        """Method get_calories_remained() applied metod get_today_remained()
+        This method returns message about the calorie limit status."""
+
         cal_remained = self.get_today_remained()
         if cal_remained > 0:
-            return (f'Сегодня можно съесть что-нибудь ещё, '
+            return ('Сегодня можно съесть что-нибудь ещё, '
                     f'но с общей калорийностью не более {cal_remained} кКал')
-        else:
-            return 'Хватит есть!'
-
+        return 'Хватит есть!'
+            
 
 class CashCalculator(Calculator):
+    """Class CashCalculator extended class Calculator.
+    Added currency exchange rate constants"""
+
     USD_RATE = 60.0
     EURO_RATE = 70.0
     RUB_RATE = 1.0
 
-    def __init__(self, limit: float) -> None:
-        super().__init__(limit)
+    def get_today_cash_remained(self, currency: str) -> str:
+        """Method get_today_cash_remained(currency) applied method get_today_remained().
+        
+        Parameters:
+        currency (str): one of the strings - 'usd', 'eur', 'rub'
+        
+        This method returns message about the status of the daily balance
+        in the supported currency."""
 
-    def get_today_cash_remained(self, currency):
         currencies = {
-            'usd': CashCalculator.USD_RATE,
-            'eur': CashCalculator.EURO_RATE,
-            'rub': CashCalculator.RUB_RATE,
+            'usd': (CashCalculator.USD_RATE, 'USD'),
+            'eur': (CashCalculator.EURO_RATE, 'Euro'),
+            'rub': (CashCalculator.RUB_RATE, 'руб'),
         }
         cash_remained = self.get_today_remained()
-        currency_type = currencies[currency]
-        currency_remained = round(cash_remained / currency_type, 2)
-        curr_print = {
-            'usd': 'USD',
-            'eur': 'Euro',
-            'rub': 'руб',
-        }
         if cash_remained == 0:
             return 'Денег нет, держись'
-        elif cash_remained > 0:
-            return (f'На сегодня осталось '
-                    f'{currency_remained} {curr_print[currency]}')
+        if currency not in currencies:
+            return f'Неподдерживаемый тип валюты {currency}'
+        rate, name = currencies[currency]
+        currency_remained = round(cash_remained / rate, 2)
+        if cash_remained > 0:
+            return f'На сегодня осталось {currency_remained} {name}'
         else:
-            return (f'Денег нет, держись: твой долг - '
-                    f'{abs(currency_remained)} {curr_print[currency]}')
-
-
-cash_calculator = CashCalculator(1000)
-
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=1000, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='сосиска'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=400,
-                                  comment='обед',
-                                  date='08.11.2019'))
-
-print(cash_calculator.get_today_cash_remained('eur'))
+            cash_remained = abs(currency_remained)
+            return ('Денег нет, держись: твой долг - '
+                    f'{cash_remained} {name}')
